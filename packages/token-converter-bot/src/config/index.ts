5dd396e59f17dc860926d15e77332f8f63124457
Corey Rice <coreyarice@gmail.com>
2024-02-05 11:17:31 -0300

    feat: add logging

--- packages/token-converter-bot/src/config/index.ts
+++ packages/token-converter-bot/src/config/index.ts
@@ -1,10 +1,22 @@
+import { SUPPORTED_CHAINS } from "./chains";
+
 const subgraphUrlByNetwork = {
   bsctestnet: "https://api.thegraph.com/subgraphs/name/pancakeswap/exchange-v3-chapel",
   bscmainnet: "https://api.thegraph.com/subgraphs/name/pancakeswap/exchange-v3-bsc",
 };
 
+const safelyGetEnvVar = <T = string>(key: keyof typeof process.env) => {
+  const envVar = process.env[key];
+  if (envVar !== undefined) {
+    return envVar as T;
+  }
+  throw new Error(`${key} not defined in environment variables`);
+};
+
 const config = {
-  subgraphUrl: subgraphUrlByNetwork[process.env.NETWORK],
+  subgraphUrl: subgraphUrlByNetwork[safelyGetEnvVar<SUPPORTED_CHAINS>("NETWORK")],
+  telegramBotToken: safelyGetEnvVar("TELEGRAM_BOT_TOKEN"),
+  telegramChatId: +safelyGetEnvVar("TELEGRAM_CHAT_ID"),
 };
 
 export default config;
