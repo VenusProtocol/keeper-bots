639e7cacbb9b61f8a01e3d4c96dd01d12eb8bdc0
Corey Rice <coreyarice@gmail.com>
2024-03-14 12:30:58 -0300

    refactor: remove baseAsset from query

--- packages/token-converter-bot/src/converter-bot/queries/read/readTokenConverterConfigs/formatTokenConverterConfigs.ts
+++ packages/token-converter-bot/src/converter-bot/queries/read/readTokenConverterConfigs/formatTokenConverterConfigs.ts
@@ -3,7 +3,6 @@ import { Address } from "viem";
 import { TokenConvertersQuery } from "../../../../subgraph-client/.graphclient";
 
 export interface TokenConverterConfig {
-  baseAsset: Address;
   tokenConverter: Address;
   tokenAddressOut: Address;
   tokenAddressIn: Address;
@@ -12,7 +11,6 @@ export interface TokenConverterConfig {
 const formatTokenConverterConfigs = (data: TokenConvertersQuery["tokenConverters"]) => {
   const configs = data.reduce((acc, curr) => {
     const formatted = curr.configs.map(c => ({
-      baseAsset: curr.baseAsset as Address,
       tokenConverter: curr.id as Address,
       tokenAddressOut: c.tokenAddressOut as Address,
       tokenAddressIn: c.tokenAddressIn as Address,
