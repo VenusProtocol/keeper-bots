51f23d805bb189efa5a488030ab301afeb220340
Corey Rice <coreyarice@gmail.com>
2024-03-18 16:26:21 -0300

    fixup! feat: add balance of command

--- packages/cli/source/queries/getBalanceOf.ts
+++ packages/cli/source/queries/getBalanceOf.ts
@@ -1,29 +1,8 @@
 import {
 	Address,
-	HttpTransport,
-	PublicClient,
-	createPublicClient,
-	http,
 	erc20Abi,
 } from 'viem';
-
-import config from '../config/index.js';
-import {chains} from '../config/chains.js';
-import type {SUPPORTED_CHAINS} from '../config/chains.js';
-
-export const getPublicClient = (): PublicClient<
-	HttpTransport,
-	(typeof chains)[SUPPORTED_CHAINS]
-> => {
-	const chainName = config.network;
-	return createPublicClient({
-		chain: chains[chainName],
-		transport: http(process.env[`LIVE_NETWORK_${chainName}`]),
-	});
-};
-
-const client: PublicClient<HttpTransport, (typeof chains)[SUPPORTED_CHAINS]> =
-	getPublicClient();
+import publicClient from './publicClient.js'
 
 const useBalanceOf = async ({
 	tokenAddress,
@@ -34,7 +13,7 @@ const useBalanceOf = async ({
 	contractAddress: Address;
 	blockNumber?: string;
 }) => {
-	const resp = client.readContract({
+	const resp = publicClient.readContract({
 		address: tokenAddress,
 		abi: erc20Abi,
 		functionName: 'balanceOf',
