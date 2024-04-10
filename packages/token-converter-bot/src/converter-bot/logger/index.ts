4dda0424e59b979932bd94bf911d4ed05798bd7b
Corey Rice <coreyarice@gmail.com>
2024-04-10 13:27:00 -0300

    refactor: remove telegram logging

--- packages/token-converter-bot/src/converter-bot/logger/index.ts
+++ packages/token-converter-bot/src/converter-bot/logger/index.ts
@@ -1,29 +1,9 @@
 import winston from "winston";
-import TelegramLogger from "winston-telegram";
-
-import config from "../../config";
 
 const logger = winston.createLogger({
   format: winston.format.combine(winston.format.json()),
   transports: [
     new winston.transports.Console(),
-    new TelegramLogger({
-      token: config.telegramBotToken,
-      chatId: config.telegramChatId,
-      silent: !!process.env.LOCAL,
-      formatMessage: ({ level, message }) => {
-        if (level === "error") {
-          return `❌ ${message}`;
-        }
-        if (level === "warning") {
-          return `⚠️ ${message}`;
-        }
-        if (level === "info") {
-          return `ℹ️ ${message}`;
-        }
-        return message;
-      },
-    }),
   ],
 });
 
