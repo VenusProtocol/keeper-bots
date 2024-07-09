dd380cdf8926ccbf1506538e8cb4a94ead31ad8f
Corey Rice <coreyarice@gmail.com>
2024-07-09 09:37:58 -0300

    fix: update token conerter package and release

--- packages/cli/source/venus.tsx
+++ packages/cli/source/venus.tsx
@@ -4,7 +4,7 @@ import "./utils/env.js";
 
 const app = new Pastel({
   importMeta: import.meta,
-  description: "CLI for interacting with the Venus Protocol and perform maintenance tasks"
+  description: "CLI for interacting with the Venus Protocol and perform maintenance tasks."
 });
 
 await app.run();
